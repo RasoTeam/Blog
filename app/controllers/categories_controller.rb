2d3b9cf1d776b0824a5c8557796d6ff7f6b16df1
Cláudio Belo Lourenço <pt.smooke@gmail.com>
2012-11-07 11:01:48 +0000

    categories controller and view created and finished 	modified:   Gemfile 	renamed:    app/assets/images/fonts/nobile.ttf -> app/assets/fonts/nobile.ttf 	renamed:    app/assets/images/fonts/nobile_bold.ttf -> app/assets/fonts/nobile_bold.ttf 	renamed:    app/assets/images/fonts/nobile_bold_italic.ttf -> app/assets/fonts/nobile_bold_italic.ttf 	renamed:    app/assets/images/fonts/nobile_italic.ttf -> app/assets/fonts/nobile_italic.ttf 	renamed:    app/assets/images/fonts/socialico.ttf -> app/assets/fonts/socialico.ttf 	modified:   app/assets/stylesheets/application.css.scss 	modified:   app/controllers/categories_controller.rb 	modified:   app/views/categories/_categories_menu.html.erb 	new file:   app/views/categories/_form.html.erb 	new file:   app/views/categories/edit.html.erb 	new file:   app/views/categories/index.html.erb 	new file:   app/views/categories/new.html.erb

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,43 @@
 class CategoriesController < ApplicationController
-  
   def show
     @category = Category.find(params[:id])
     @posts = @category.posts.order("created_at DESC").paginate(:page => params[:page], :per_page => 4)
   end
 
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(params[:category])
+    if @category.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+
+    if @category.update_attributes(params[:category])
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to categories_path
+  end
 end
