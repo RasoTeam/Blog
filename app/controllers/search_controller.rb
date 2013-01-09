a442970aaf283af2708ed94164a6bdbe05a57716
Cláudio Belo Lourenço <pt.smooke@gmail.com>
2013-01-09 22:15:09 +0000

    small details

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
 class SearchController < ApplicationController
   def search
     s = params[:query]
-    @posts = Post.find(:all, :conditions => ['content LIKE ?', '%' + s + '%'])
+    @posts = Post.where('content LIKE ? OR title LIKE ?', '%' + s + '%','%' + s + '%').paginate(:page => params[:page], :per_page => 4)
+    #find(:all, :conditions => ['content LIKE ?', '%' + s + '%']).paginate(:page => params[:page], :per_page => 4)
   end
 end
